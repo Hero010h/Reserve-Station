cfb3427774d7f4647b9ed7ae9e267a15d81dc4ae
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-11-08 23:39:10 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Reserve/EntityEffects/Effects/SpaceAdaptation.cs
+++ Content.Server/_Reserve/EntityEffects/Effects/SpaceAdaptation.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Hero010h <163765999+Hero010h@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Body.Components;
 using Content.Server.Body.Systems;
 using Content.Shared._Shitmed.Body.Organ;
